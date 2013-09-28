c6245b5c224c825a7bf17281152a3692668c1f73
Adam Childs <adchilds@eckerd.edu>
2013-09-28 00:06:55 -0500

    Added another user role

--- src/main/java/com/adamchilds/daycare/entity/user/enumeration/UserRole.java
+++ src/main/java/com/adamchilds/daycare/entity/user/enumeration/UserRole.java
@@ -4,7 +4,8 @@ public enum UserRole {
     ROLE_USER(0),
     ROLE_ACCOUNT_OWNER(1),
     ROLE_EMPLOYEE(2),
-    ROLE_ADMINISTRATOR(3);
+    ROLE_ADMINISTRATOR(3),
+    ROLE_SUPERUSER(4);
 
     private int roleId;
 
