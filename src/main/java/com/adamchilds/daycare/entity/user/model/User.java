84f01edbbea9aac895671c53f96a97f7d30aff2d
Adam Childs <adchilds@eckerd.edu>
2013-09-29 11:53:52 -0500

    No longer encrypting the user's password in the set method. This is troublesome for form validation. Encrypt later before persisting entity to the database.

--- src/main/java/com/adamchilds/daycare/entity/user/model/User.java
+++ src/main/java/com/adamchilds/daycare/entity/user/model/User.java
@@ -82,7 +82,7 @@ public class User {
     }
 
     public void setPassword(String password) {
-        this.password = Encryption.encodeString(password);
+        this.password = password;
     }
 
     public String getEmailAddress() {
